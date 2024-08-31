a5959518a5c9fb7b2f98c93aedcb66c76b6b02c1
narae3759 <narae3759@gmail.com>
2024-08-31 18:57:15 +0900

    Feat: 라우터 생성
    - 기본 crud 라우터 생성
    - 프로필 라우터 추가
    - langchain 라우터 추가

---
+++ app/main.py
@@ -0,0 +1,28 @@
+# > uvicorn app.main:app --host 0.0.0.0 --port 8502 --reload
+import uvicorn 
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.core.config import configs
+from app.api.v1.api import api_router
+
+app = FastAPI(**configs.fastapi)
+app.include_router(api_router)
+
+# CORS 보안 처리
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      # 허용할 도메인 리스트
+    allow_credentials=True,     # 자격증명 허용 여부
+    allow_methods=["*"],        # 허용할 메서드 리스트 
+    allow_headers=["*"]         # 허용할 HTTP 헤더
+)
+
+@app.get("/")
+def root():
+    return {"hello": "Python"}
+
+@app.post("/chat", summary="AI 답변을 생성합니다")
+def chat():
+    return {"hello": "Python"}
